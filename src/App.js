9db73af72e6b0dc48cce0b504bfc115c8f554e45
jacko110 <amirseifu6@gmail.com>
2022-10-01 08:12:33 -0400

    fetch news from algolia API

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import Stories from './Stories'
+import Buttons from './Buttons'
+function App() {
+  return(
+    <>
+      <SearchForm/>
+      <Stories/>
+      <Buttons/>
+    </>
+  )
+}
+
+export default App
